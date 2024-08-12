0d70972987b2426cd0284fbad68e433dbc78ab46
bhanuprasad-mekala <bhanuprasad.mekala@outlook.com>
2024-08-12 06:06:26 +0530

    View-bills

---
+++ src/Model/ViewBill.ts
@@ -0,0 +1,12 @@
+export interface ViewBill{
+    CustomerNo:string,
+    CustomerName:string,
+    DOB:string,
+    Address:string,
+    PinCode:number,
+    Month:string,
+    year:number,
+    Noofunit:number,
+    Amount:number
+
+}
